f87f6626c29901fd53c9a7fa42c8b59fc1426188
rishj2612 <>
2026-03-24 12:03:09 +0530

    added routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="alert alert-info">
+            <h4>Next.js Training Application</h4>
+            <p>Application to demonstrate the feature of React and Next.js</p>
+        </div>
+    )
+}
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function AppBar() {
+    return (
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="/">
+                        Next.js
+                    </Link>
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/login">Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
